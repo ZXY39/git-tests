2dac84aa598aacfdaa3fe784458eb6650f7e49b6
ZGASQ <zgasq39@qq.com>
2022-10-15 22:39:09 +0800

    idea test hf

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -4,5 +4,8 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
     }
 }
